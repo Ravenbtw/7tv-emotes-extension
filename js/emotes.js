733cc728e6662621ab15942511cc62877e7a238c
Ravenbtw <ravenbtw@protonmail.com>
2022-04-12 22:56:47 +0100

    Complete redesign.

--- js/emotes.js
+++ js/emotes.js
@@ -1,26 +1,33 @@
 const emotesDiv = document.querySelector('.emotes');
-const titleDiv = document.querySelector('.title');
+const messageDiv = document.querySelector('.message');
 
-window.Twitch.ext.onAuthorized(auth => {
-    fetch(`https://api.7tv.app/v2/users/${auth.channelId}/emotes`)
-    .then(res => res.json())
-    .then(data => {
-        emotesDiv.innerHTML = '';
-        if (!data.Status && data.Status !== 404) {
-            data.forEach(emote => {
-                emotesDiv.insertAdjacentHTML('beforeend', `<div class="col-4 emote"><a href="https://7tv.app/emotes/${emote.id}" class="text-white text-decoration-none" target="_blank"><img src="${emote.urls[0][1]}" alt="${emote.name}" class="emote-image"><p class="emote-name">${emote.name}</p></a></div>`)
-            });
-        } else {
-            emotesDiv.insertAdjacentHTML('beforeend', '<h1 class="display-4">No emotes found on this channel.</h1>')
-        }
-    });
-});
+const getEmotes = async (id) => {
+  try {
+    const response = await fetch(`https://api.7tv.app/v2/users/${id}/emotes`);
 
-window.Twitch.ext.configuration.onChanged(() => {
-    if (window.Twitch.ext.configuration.broadcaster) {
-        const config = JSON.parse(window.Twitch.ext.configuration.broadcaster.content);
-        if (config.headerText) titleDiv.innerText = config.headerText;
-        if (/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(config.textColor)) document.documentElement.style.setProperty('--text-color', config.textColor);
-        if (/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(config.backgroundColor)) document.documentElement.style.setProperty('--background-color', config.backgroundColor);
+    const emotes = await response.json();
+
+    emotesDiv.innerHTML = '';
+
+    for (const { id, name, urls } of emotes) {
+      emotesDiv.insertAdjacentHTML(
+        'beforeend',
+        `
+        <a href="https://7tv.app/emotes/${id}" class="emote" target="_blank">
+          <div class="emote-image-container">
+            <img src="${urls[3][1]}" alt="${name}" />
+          </div>
+          <p>${name}</p>
+        </div>
+        `
+      );
     }
-});
+
+    messageDiv.innerText = '';
+  } catch (error) {
+    console.log(error);
+    messageDiv.innerText = 'Failed to load emotes.';
+  }
+};
+
+window.Twitch.ext.onAuthorized(({ channelId }) => getEmotes(channelId));
