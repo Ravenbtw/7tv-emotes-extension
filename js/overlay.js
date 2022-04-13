f1ae400e4e21ec53f02f8580527573c09ec42554
Ravenbtw <ravenbtw@protonmail.com>
2022-04-13 16:59:13 +0100

    Styling changes and added overlay page.

---
+++ js/overlay.js
@@ -0,0 +1,85 @@
+const container = document.querySelector('.container');
+const eventAction = document.querySelector('.event-action');
+const emoteImage = document.querySelector('.emote-image');
+const emoteName = document.querySelector('.emote-name');
+
+const emoteImages = {};
+
+let eventTimeout;
+let imageTimeout;
+
+const displayAction = (add, name, image, id) => {
+  clearTimeout(eventTimeout);
+  clearTimeout(imageTimeout);
+
+  container.style.opacity = 1;
+
+  eventTimeout = setTimeout(() => {
+    container.style.opacity = 0;
+
+    imageTimeout = setTimeout(() => {
+      emoteImage.src = '';
+    }, 1000);
+  }, 1000 * 5);
+
+  switch (add) {
+    case true:
+      emoteImages[id] = image;
+      eventAction.innerText = 'Emote Added';
+      emoteImage.src = image;
+      break;
+    case false:
+      eventAction.innerText = 'Emote Removed';
+      emoteImage.src = emoteImages[id];
+      break;
+  }
+
+  emoteName.innerText = name;
+};
+
+const getEmotes = async (id) => {
+  try {
+    const response = await fetch(`https://api.7tv.app/v2/users/${id}/emotes`);
+
+    const emotes = await response.json();
+
+    emotes.forEach(({ id, urls }) => (emoteImages[id] = urls[3][1]));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const listenToEvents = async (id) => {
+  try {
+    const response = await fetch(`https://api.7tv.app/v2/users/${id}`);
+
+    const user = await response.json();
+
+    const source = new EventSource(
+      `https://events.7tv.app/v1/channel-emotes?channel=${user.login}`
+    );
+
+    source.addEventListener('update', ({ data }) => {
+      const { action, name, emote, emote_id } = JSON.parse(data);
+
+      displayAction(
+        action === 'ADD',
+        name,
+        action === 'ADD' ? emote.urls[3][1] : null,
+        emote_id
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getEmotes('729353699');
+
+listenToEvents('729353699');
+
+window.Twitch.ext.onAuthorized(async ({ channelId }) => {
+  getEmotes(channelId);
+
+  listenToEvents(channelId);
+});
